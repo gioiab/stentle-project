2eec730e01b8b5082f76ab6181c33c0967bc5e0a
Gioia Ballin <gioia.ballin@satispay.com>
2015-11-05 23:57:51 +0100

    initialize a simple spring boot project with gradle

---
+++ src/main/java/com.stentle/Application.java
@@ -0,0 +1,16 @@
+package com.stentle;
+
+/**
+ * Created by gioiaballin on 05/11/15.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
