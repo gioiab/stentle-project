8321e9d0ac6e52f87543b2512b78d44a7be5f7c3
Gioia Ballin <gioia.ballin@satispay.com>
2015-11-08 00:27:56 +0100

    adding annotations for validation purposes

--- src/main/java/com/stentle/Application.java
+++ src/main/java/com/stentle/Application.java
@@ -4,13 +4,27 @@ package com.stentle;
  * Created by gioiaballin on 05/11/15.
  */
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-@SpringBootApplication
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
+
 }
